e0b5346766395ba8fddc1b9784f6005aab130df7
tohid.abedini <tohid.abedini>
2024-05-28 17:37:11 +0330

    [Add] run.sh script

---
+++ human_eval/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+directories=("./data/jsonls/joined_jsonls.jsonl"
+"./data/jsonls/eval_instances_tulu_1.jsonl"
+"./data/jsonls/llama3_alpaca_4_2048_5e-7_persian_system_prompt_vs_llama3_alpaca_4_2048_persian_system_prompt.jsonl"
+"./data/jsonls/llama3_alpaca_4_2048_5e-7_persian_system_prompt_vs_Part V1.jsonl"
+"./data/jsonls/llama3_alpaca_4_2048_persian_system_prompt_vs_Part V1.jsonl"
+"./data/jsonls/llama3_alpaca_4_2048_vs_llama3_base.jsonl"
+"./data/jsonls/llama3_alpaca_4_2048_vs_Part V1.jsonl"
+"./data/jsonls/llama3_base_persian_system_prompt_vs_llama3_alpaca_4_2048_5e-7_persian_system_prompt.jsonl"
+"./data/jsonls/llama3_base_persian_system_prompt_vs_llama3_alpaca_4_2048_persian_system_prompt.jsonl"
+"./data/jsonls/llama3_base_persian_system_prompt_vs_llama3_base.jsonl"
+"./data/jsonls/llama3_base_persian_system_prompt_vs_Part V1.jsonl"
+"./data/jsonls/llama3_base_vs_Part V1.jsonl")
+
+
+directory_index=0
+port=5001
+
+while getopts "p:d:" opt; do
+  case $opt in
+    p) port="$OPTARG"
+    ;;
+    d) directory_index="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+directory=${directories[$directory_index]}
+
+echo "[directory_index: $directory_index, directory: $directory, port: $port]"
+
+
+python app.py --comparison_data_path "${directory}" --port $port
+
+# ./run.sh -d 0 -p 5001
+
+# 1 6 8
